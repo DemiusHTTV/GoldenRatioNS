67f4fde39c81fb8e806e28c6fb350489f60ddc15
Sanya <sasabuzinaev@gmail.com>
2026-06-22 15:11:13 +0800

    feat: added basic structure of datatype

---
+++ src/Logos/main.py
@@ -0,0 +1,19 @@
+
+class Logos:
+    def __init__(self, from_: int) -> None:
+        self.from_ = from_
+
+    
+    def __str__(self) -> str:
+        return f"Logos(from_={self.from_})"
+    
+    
+    def __sub__(self)->None:
+        pass
+
+
+    def __add__(self)->None:
+        pass
+
+    def __mul__(self)->None:
+        pass
