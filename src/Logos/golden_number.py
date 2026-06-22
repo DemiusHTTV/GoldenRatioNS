1e8c638150fd2e2fa6f2fba1d8f22d4d8e8ff9af
Yana <anaermakova948@gmail.com>
2026-06-22 20:29:27 +0800

    feat: add power computation for phi and string representation

--- src/Logos/golden_number.py
+++ src/Logos/golden_number.py
@@ -6,6 +6,9 @@ class GoldenNumber:
         self.a=a
         self.b=b
 
+    def __str__(self) -> str:
+        return f"{self.a} + {self.b}·√5"
+
     def __add__(self,other:"GoldenNumber")->"GoldenNumber":
         new_a=self.a+other.a
         new_b=self.b+other.b
@@ -32,6 +35,32 @@ class GoldenNumber:
             return int(self.a)
         else:
             raise ValueError("Cannot convert to int")
+        
+    
+FI_POS=GoldenNumber(Fraction(1,2),Fraction(1,2))
+FI_NEG=GoldenNumber(Fraction(-1,2),Fraction(1,2))
+ONE = GoldenNumber(Fraction(1), Fraction(0))
+def fi_degree(n:int)->GoldenNumber:
+    if n == 0:
+        return ONE
+    prev = ONE
+    if n < 0:
+        curr = FI_NEG
+        for _ in range(1, -n):
+            temp = curr + prev
+            prev = curr
+            curr = temp
+    else:
+        curr = FI_POS
+        for _ in range(2, n + 1):
+            temp = curr + prev
+            prev = curr
+            curr = temp
+    return curr
+
+
+
+
 
     
 
