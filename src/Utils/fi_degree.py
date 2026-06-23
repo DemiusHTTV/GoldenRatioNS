46c52c5dfc8364371c26308115f2a01be795a7be
Dmitriy <babygreenyoda@yandex.ru>
2026-06-24 03:34:59 +1000

    feat: added the basic normalization algorithm
    without data transfer

--- src/Utils/fi_degree.py
+++ src/Utils/fi_degree.py
@@ -1,23 +1,26 @@
 from Logos import GoldenNumber
+from fractions import Fraction
 
 class FiCalculationDegree:
+  
+    def __init__(self):
+        self.FI_POS=GoldenNumber(Fraction(1,2),Fraction(1,2))
+        self.FI_NEG=GoldenNumber(Fraction(-1,2),Fraction(1,2))
+        self.ONE = GoldenNumber(Fraction(1), Fraction(0))
 
-    FI_POS=GoldenNumber(Fraction(1,2),Fraction(1,2))
-    FI_NEG=GoldenNumber(Fraction(-1,2),Fraction(1,2))
-    ONE = GoldenNumber(Fraction(1), Fraction(0))
-
-    def fi_degree(n:int)->GoldenNumber:
+ 
+    def fi_degree(self,n:int)->GoldenNumber:
         if n == 0:
-            return ONE
-        prev = ONE
+            return self.ONE
+        prev = self.ONE
         if n < 0:
-            curr = FI_NEG
+            curr = self.FI_NEG
             for i in range(1, -n):
                 temp = curr + prev
                 prev = curr
                 curr = temp
         else:
-            curr = FI_POS
+            curr = self.FI_POS
             for i in range(2, n + 1):
                 temp = curr + prev
                 prev = curr
