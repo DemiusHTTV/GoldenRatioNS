64e30e01fe9ac8bcc6356e1d60672aa60653033d
Dmitriy <babygreenyoda@yandex.ru>
2026-06-24 01:46:06 +1000

    chore: delete extra utils code

--- src/Logos/golden_number.py
+++ src/Logos/golden_number.py
@@ -1,6 +1,5 @@
 from fractions import Fraction
-
-
+from .Utils import Normalization
 class GoldenNumber:
     def __init__(self,a:Fraction,b:Fraction)-> None:
         self.a=a
@@ -12,6 +11,7 @@ class GoldenNumber:
     def __add__(self,other:"GoldenNumber")->"GoldenNumber":
         new_a=self.a+other.a
         new_b=self.b+other.b
+
         return GoldenNumber(new_a,new_b)
 
     def __sub__(self,other:"GoldenNumber")->"GoldenNumber":
@@ -35,25 +35,3 @@ class GoldenNumber:
             return int(self.a)
         else:
             raise ValueError("Cannot convert to int")
-        
-    
-FI_POS=GoldenNumber(Fraction(1,2),Fraction(1,2))
-FI_NEG=GoldenNumber(Fraction(-1,2),Fraction(1,2))
-ONE = GoldenNumber(Fraction(1), Fraction(0))
-def fi_degree(n:int)->GoldenNumber:
-    if n == 0:
-        return ONE
-    prev = ONE
-    if n < 0:
-        curr = FI_NEG
-        for _ in range(1, -n):
-            temp = curr + prev
-            prev = curr
-            curr = temp
-    else:
-        curr = FI_POS
-        for _ in range(2, n + 1):
-            temp = curr + prev
-            prev = curr
-            curr = temp
-    return curr
