7981d3248543ad3c1ea7a95ef653a2fa85ac9d73
Dmitriy <babygreenyoda@yandex.ru>
2026-06-24 01:15:15 +1000

    feat: added normalization class and moved main.py to upper lvl

---
+++ src/Utils/normalization.py
@@ -0,0 +1,9 @@
+import FiCalculationDegree
+import .Logos.GoldenNumber
+
+class Normalization:
+    ''' класс для нормализациии ответа при 11 22 и тд'''
+    
+    def __init__(self, a:GoldenNumber,b:GoldenNumber): -> GoldenNumber
+        a , b = a.self , b.self
+        
