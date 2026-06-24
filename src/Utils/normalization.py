8862bdcc2f773ea800651228975de79aaea0f604
Dmitriy <babygreenyoda@yandex.ru>
2026-06-25 03:14:22 +1000

    feat: added translater goldenRatio type to dict

--- src/Utils/normalization.py
+++ src/Utils/normalization.py
@@ -2,14 +2,14 @@ import FiCalculationDegree
 from Logos import GoldenNumber
 
 class Normalization:
-    ''' класс для нормализациии ответа при 11 22 и тд'''
+    ''' класс для нормализациии ответа при 11 2 и тд'''
     
     def __init__(self, a:GoldenNumber,b:GoldenNumber)-> None: 
        self.a = self.normalize(a)
        self.b = self.normalize(b)
 
     
-@staticmethod
+
     def normalize(digits: list) -> list:
 
         changed = True
