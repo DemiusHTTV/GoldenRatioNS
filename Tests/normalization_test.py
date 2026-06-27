7ab0021d2264886a6bf8ca5d838a6c3a8b7840bf
Dmitriy <babygreenyoda@yandex.ru>
2026-06-27 18:15:01 +1000

    test: testing int numbers & change configuration
    test - passed

--- Tests/normalization_test.py
+++ Tests/normalization_test.py
@@ -1,6 +1,6 @@
 import pytest
 from fractions import Fraction
-from Normalization import Normalization
+from Utils.normalization import Normalization
 from Logos import GoldenNumber
 
 
@@ -8,3 +8,10 @@ def test_normalize_empty_dict():
     result = Normalization.normalize({})
     assert result == {}
 
+def test_normalize_number():
+    n = GoldenNumber(Fraction(11),Fraction(0))
+    m = GoldenNumber(Fraction(4), Fraction(0))
+    result11 = Normalization(n).digits
+    result4 = Normalization(m).digits
+    assert result11 == {4: 1, 2: 1, 0: 1, -2: 1, -4: 1}
+    assert result4 ==  {2: 1, 0: 1, -2: 1}
