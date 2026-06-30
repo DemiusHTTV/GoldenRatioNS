efd3ea1505fa7097c77fe615c8d87924056ed319
Yana <anaermakova948@gmail.com>
2026-06-30 15:38:03 +0800

    feat:added function for translate to fi

--- src/goldenratio/__init__.py
+++ src/goldenratio/__init__.py
@@ -1,5 +1,5 @@
 from goldenratio.Logos.golden_number import GoldenNumber
-  
+
  
 def normalize(value: GoldenNumber):
      from goldenratio.Utils.normalization import Normalization 
@@ -8,3 +8,7 @@ def normalize(value: GoldenNumber):
 def fi_degree(n: int) -> GoldenNumber:
     from goldenratio.Utils.fi_degree import FiCalculationDegree
     return FiCalculationDegree().fi_degree(n)
+
+def translate_to_fi(value:GoldenNumber):
+     from goldenratio.Utils.fi_base import PhiBase
+     return PhiBase.from_golden_number(value)
